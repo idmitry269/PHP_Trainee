92d3fa2db052848a3097a080677700eae7458dac
idmitry269 <70436265+idmitry269@users.noreply.github.com>
2020-10-10 09:52:33 +0300

    Основные страницы

---
+++ profile.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="ru" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="no-cache">
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/style.css">
+    <title>MyBlog</title>
+</head>
+
+<body class="d-flex flex-column h-100">
+
+<header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">MyBlog</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarsExample03">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Авторизация</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="registration.php">Регистрация</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="gradient-top"></div>
+
+</header>
+
+
+
+<main role ="main" class="flex-shrink-0">
+    <div class="container">
+        <div class="row">
+            <div class="navigation">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="news.php">Новости</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" >Юмор</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" >Истории</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" >Путешествия</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" >Музыка</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" >Видео</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Игры</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" >Гифки</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="content">
+            </div>
+        </div>
+    </div>
+</main>
+
+<footer class="footer mt-auto">
+    <div class="gradient-bottom"></div>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container">
+
+            <div class="nav-item mr-auto">
+                <a class="nav-link" href="#">Наверх</a>
+            </div>
+            <div class="nav-item-right">
+                <a class="nav-link">Контакты</a>
+            </div>
+        </div>
+
+    </nav>
+</footer>
+
+<script src="js/jquery-3.5.1.min.js" ></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
